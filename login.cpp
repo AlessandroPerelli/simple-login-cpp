607c581cca75a149eee10e7c41e6161fa9d94f2e
AlessandroPerelli <alessandroperelli156@gmail.com>
2023-04-10 23:30:34 +0100

    added function getUserCredentials

--- login.cpp
+++ login.cpp
@@ -1,8 +1,22 @@
-#include <iostream>;
-#include <string>;
+#include <iostream>
+#include <string>
 
 using namespace std;
 
+string getUserCredentials() {
+    string username;
+    string password;
+    cout << "Enter your username: ";
+    cin >> username;
+    cout << "Enter your password: ";
+    cin >> password;
+    return username + " " + password;
+}
+
 int main() {
+    string boss;
+    cout << "Welcome to the login system" << endl;
+    cin >> boss;
+    cout << getUserCredentials() << endl;
     return 0;
 }
